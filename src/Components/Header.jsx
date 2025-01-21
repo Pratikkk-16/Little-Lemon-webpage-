c55bc3a52fe9fd873511caa632d8652acc09140c
Pratikkk-16 <shahpratik200416@gmail.com>
2025-01-21 15:28:50 +0530

    Third

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,5 +1,13 @@
+import LightLogo from "../assets/LightLogo.png"
 function Header()
 {
-
+    return(
+        <section>
+            <div className="Image">
+                <img src={LightLogo} alt="logo" className="logo"/>
+            </div>
+            <h1 className="res-logo">Little Lemon</h1>
+        </section>
+    );
 }
 export default Header

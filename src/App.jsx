0524b5a451db5504899c9c4bab325697e4b2f4fc
Pratikkk-16 <shahpratik200416@gmail.com>
2025-01-21 14:28:25 +0530

    second

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,19 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Header from './Components/Header'
+import Nav from './Components/Nav'
+import Main from './Components/Main'
+import Footer from './Components/Footer'
 function App() {
-
+    return(
+        <>
+            <Header/>
+            <Nav/>
+            <Main/>
+            <Footer/>
+        </>
+    )
 }
 
 export default App
